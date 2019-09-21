f7cc9297b9f8937f74b532eb05af0547b1a6722f
Jeremy Purser <jeremypurser@gmail.com>
2019-09-21 19:30:26 -0400

    Add newrelic and change port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8081;
+const port = 8080;
 
 // Middlewares
 app.use(express.static(path.join(__dirname, '../public')));
